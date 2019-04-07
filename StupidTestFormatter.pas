eefc040039f00ece11264280dfd20a8c6c6eedcd
leduykhongngu <leduykhongngu@gmail.com>
2019-04-07 14:19:01 +0700

    Update StupidTestFormatter.pas

--- StupidTestFormatter.pas
+++ StupidTestFormatter.pas
@@ -1,14 +1,27 @@
 {$coperators on}
 {$mode objfpc}
 {$H+}
-uses SysUtils,windows,crt;
+uses SysUtils,windows,CustApp;
+const
+        BUG='sugoichottomatte';
+type    TArr=array of string;
 var
+        exINP : TArr;
+        exOUT : TArr;
         path    : string;
         nam     : string;
         resPath : string;
-        exINP,exOUT: string;
+        App: TCustomApplication;
+procedure init;
+begin
+        exINP:=TArr.Create('inp','in');
+        exOUT:=TArr.Create('out','ans','ok','output','a');
+end;
+function param(s:string):string;
+begin
+        exit(App.GetOptionValue(s));
+end;
 function ToString(x:longint):string;
-var     s:ansistring;
 begin
         result:=IntToStr(x);
         while (length(result)<3) do result:='0'+result;
@@ -22,52 +35,101 @@ begin
                 end;
         exit(true);
 end;
+function inside(var x:TArr; s:string):boolean;
+var     i:longint;
+begin
+        for i:=0 to high(x) do
+                if (s=x[i]) then exit(true);
+        exit(false);
+end;
+function checkIn(s:string):boolean;
+begin
+        delete(s,1,1);
+        if (App.HasOption('exI')) and (s=param('exI')) then exit(true);
+        exit(inside(exINP,s));
+end;
+function CheckOut(s:string):boolean;
+begin
+        delete(s,1,1);
+        if (App.HasOption('exO')) and (s=param('exO')) then exit(true);
+        exit(inside(exOUT,s));
+end;
+function GetEx(s:string):string;
+var     i:longint;
+begin
+        for i:=length(s) downto 1 do
+                if s[i]='.' then
+                        exit(copy(s,i,length(s)-i+1));
+        exit(BUG);
+end;
 procedure Solve();
 var     Info    : TSearchRec;
         s       : string;
+        ex      : string;
         nxt     : string;
         nani    : boolean;
 begin
-        if (FindFirst(path+'*'+exINP,faAnyFile,Info)=0) then begin
+        if (FindFirst(path+'*.*',faAnyFile,Info)=0) then begin
                 repeat
                         s:=Info.Name;
-                        delete(s,length(s)-length(exINP)+1,length(exINP));
+                        ex:=GetEx(s);
+                        if (ex=BUG) then continue;
+                        delete(s,length(s)-length(ex)+1,length(ex));
                         nxt:=resPath+'TEST_'+s+'\';
-                        if not check(nxt) then break;
-                        writeln('coping ',path+s+exINP, ' to ',nxt+nam+'.INP');
-                        if (not windows.copyfile(Pchar(path+s+exINP),Pchar(nxt+nam+'.INP'),nani)) then
-                                writeln('loi');
-
-                        writeln('coping ',path+s+exOUT, ' to ',nxt+nam+'.OUT');
-                        if (not windows.copyfile(Pchar(path+s+exOUT),Pchar(nxt+nam+'.OUT'),nani)) then
-                                writeln('loi');
+                        if (CheckIn(ex)) then begin
+                                if not check(nxt) then break;
+                                if (windows.copyfile(Pchar(path+s+ex),Pchar(nxt+nam+'.INP'),nani)) then
+                                        writeln('copied '+path+s+ex+' to ' + nxt+nam+'.INP')
+                                else    writeln('error '+path+s+ex+' to ' + nxt+nam+'.INP');
+                        end
+                        else if (CheckOut(ex)) then begin
+                                if not check(nxt) then break;
+                                if (windows.copyfile(Pchar(path+s+ex),Pchar(nxt+nam+'.OUT'),nani)) then
+                                        writeln('copied '+path+s+ex+' to ' + nxt+nam+'.OUT')
+                                else    writeln('error '+path+s+ex+' to ' + nxt+nam+'.OUT');
+                        end;
                 until FindNext(Info)<>0;
         end;
         sysutils.FindClose(Info);
         writeln('done');
 end;
 begin
-        clrscr;
+        App:=TCustomApplication.create(nil);
         writeln('Stupid ''Test Formatter'' by Canoccancap');
-        write('Nhap duong dan toi folder test can chuyen: ');
-        readln(path);
+        Init;
+        //Get Path of test
+        if (App.HasOption('i')) then begin
+                assign(input,param('i')); 
+                reset(input);
+        end
+        else if (not App.HasOption('l')) then
+                write('Nhap duong dan toi folder test can chuyen: ');
+        if (App.HasOption('l')) then 
+                path:=param('l')
+        else 
+                readln(path);
         if (path[length(path)]<>'\') then path+='\';
-        write('Nhap ten bai: ');
-        readln(nam);
+
+        // Get name of problem
+        if (not App.HasOption('i') and not App.HasOption('n')) then 
+                write('Nhap ten bai: ');
+        if (App.HasOption('n')) then 
+                nam:=param('n')
+        else 
+                readln(nam);
         resPath:=path;
+        nam:=upcase(nam);
+        writeln(nam);
         writeln('Cac test moi duoc tao se nam trong: ',resPath);
+        // For Log
+        if (App.HasOption('o')) then begin
+                assign(output,param('o')); 
+                rewrite(output);
+        end;
+
+        //
         if not check(ResPath) then begin
-                readln;
                 exit;
         end;
-        write('Nhap extention cua input (INP, IN hay gi do) : ');
-        readln(exINP);
-        if (exINP[1]<>'.') then exINP:='.'+exINP;
-        write('Nhap extention cua answer: ');
-        readln(exOUT);
-        if (exOUT[1]<>'.') then exOUT:='.'+exOUT;
-        //writeln('input la: ',path+nam+'\'+exinp);
         Solve();
-        readln;
 end.
-
